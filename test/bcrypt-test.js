d092544df7dd2397494e32bec07528fbb4bc9a5c
Prasanth Raja Sekaran <prasanth.dps@gmail.com>
2022-06-28 08:51:31 -0400

    Update bcrypt-test.js

--- test/bcrypt-test.js
+++ test/bcrypt-test.js
@@ -4,10 +4,10 @@ const saltRounds = 12;
 let hash1, hash2;
 
 let salt = bcrypt.genSaltSync(1);
-hash1 = bcrypt.hashSync('prasanth', salt);
-hash2 = bcrypt.hashSync('shana', 10);
+hash1 = bcrypt.hashSync('dX23%23q*', salt);
+hash2 = bcrypt.hashSync('@#%^68fds', 10);
 
 console.log("-----hashed---------")
 
-val1 = bcrypt.compareSync('prasanth', hash1);
+val1 = bcrypt.compareSync('dX23%231q*', hash1);
 console.log(val1)
